d5f7653896989eb8eb3fe5bd5d4ace24dc69e7cb
thongster <106802301+thongster@users.noreply.github.com>
2025-10-09 11:39:24 +0700

    added error conditionals

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // if negative or 0, send message ERROR
+    if ((a < 0 || b < 0) || !Number.isInteger(a) || !Number.isInteger(b) || typeof a != "number" || typeof b != "number") {
+        return "ERROR";
+    };
 
+    // check if which int is bigger
+    if (a > b) {
+
+    }
+
+    // bigger int gets count down from until variable equals lower int
+
+    // sum up all integers
 };
 
 // Do not edit below this line
