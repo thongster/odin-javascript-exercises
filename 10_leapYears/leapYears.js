df11fe8055ac5d02b54b1a021fe76f466d1eca26
thongster <106802301+thongster@users.noreply.github.com>
2025-10-09 14:18:44 +0700

    if statement for leapyears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // must be divisble by 4
+    // not divisible by 100
+    // if divisible by 400 then true
 
+    if (year % 100 === 0 && year % 400 != 0) {
+        return false;
+    } else if (year % 4 === 0 || year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+console.log(leapYears(1900));
 
 // Do not edit below this line
 module.exports = leapYears;
