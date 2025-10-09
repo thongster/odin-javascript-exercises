a9fa206f918a20ef398960c50e3cb2eb5a478b4c
thongster <106802301+thongster@users.noreply.github.com>
2025-10-09 09:46:25 +0700

    finished repeat string and reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split("");
+    arr = arr.reverse();
+    let newString = arr.join("");
+    return newString;
 };
+
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
