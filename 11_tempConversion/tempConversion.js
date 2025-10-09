8cbe89ad40553c02480c228ac559d229be16f4ca
thongster <106802301+thongster@users.noreply.github.com>
2025-10-09 14:34:16 +0700

    finished C and F conversion formulas

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  // C = (F - 32) x 5 / 9
+  let cTemp = (fTemp - 32) * 5 / 9;
+  cTemp = Math.round(cTemp * 10) / 10;
+  return cTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  // F = (C x 9 / 5) + 32
+  let fTemp = (cTemp * 9 / 5) + 32;
+  fTemp = Math.round(fTemp * 10) / 10;
+  return fTemp;
 };
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(100));
 
 // Do not edit below this line
 module.exports = {
