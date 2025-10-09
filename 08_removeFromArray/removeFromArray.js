6cf2961fde2fe7df9799a9b79458d39a5d234547
thongster <106802301+thongster@users.noreply.github.com>
2025-10-09 10:13:15 +0700

    passed first test on removefrom array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, value) {
+    for (i = 0; i < arr.length; i++) {
+        let index = arr.indexOf(value);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+    };
+    return arr;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
